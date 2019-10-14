6aa5e3d1e0c0f6c792d0120af4546996bda5e797
Zaska Seignon <zseignon@z2r1p1.le-101.fr>
2019-10-14 10:58:04 +0200

    pathfinding v0.5

--- inc/pathfinding.h
+++ inc/pathfinding.h
@@ -6,7 +6,7 @@
 /*   By: zseignon <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 14:30:42 by zseignon     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 14:35:27 by zseignon    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/11 15:44:55 by zseignon    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,11 +14,19 @@
 #ifndef PATHFINDING_H
 # define PATHFINDING_H
 
-typedef struct			s_cost
+typedef struct			s_ant
 {
-	int					d_start;
-	int					d_end;
-	int					total;
-}						t_cost;
+	int					*used;
+	int					*roadmap;
+	int					pos;
+}						t_ant;
+
+typedef struct			s_colony
+{
+	struct s_ant		**ant;
+	size_t				nb;
+}						t_colony;
+
+t_colony				ant_spawn
 
 #endif
--- src/pathfinding.c
+++ src/pathfinding.c
@@ -6,32 +6,61 @@
 /*   By: zseignon <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 14:30:52 by zseignon     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 15:45:27 by zseignon    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/14 10:56:36 by zseignon    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "pathfinding.h"
 
-void				calc_dstart(t_cost *tab, int **matrix)
+static int		eval(t_ant **reached, t_anthill *ah)
 {
-	int					*used;
-	size_t				pos;
-
-	if (!(used = ft_memalloc(sizeof(int) * len)))
-		return (-1);
-	pos = 0;
 
 }
 
-t_roadset			*pathfinding(t_anthill *ah, int **matrix)
+static void		ant_loop(t_ant **ant, t_ant **reached, size_t *nb_ant)
 {
-	t_cost				*tab;
+	char			end_reached;
+	size_t			n;
+	
+	n = 0;
+	end_reached = 0;
+	while (*nb_ant > 0 && !end_reached)
+	{
+		if ((tubes = (tube_count(matrix, ant[n].pos)) - 1) > 1)
+			ant_dup(ant, n, tubes);
+		else if (tubes == 0)
+			if ((ant_rewind(ant, n, matrix)) == -1)
+				ant_kill(ant, n, matrix, nb_ant);
+		ant_scout(ant, n, matrix);
+		if (ant[n].pos == 1)
+		{
+			reached_add(ant, reached, n);
+			*nb_ant -= 1;
+			end_reached = 1;
+		}
+		else
+			n += 1;
+	}
+}
 
-	if (!(tab = (t_cost *)malloc(sizeof(t_cost) * ah->nb_room)))
-		return (-1);
-	ft_bzero(tab, sizeof(t_cost) * ah->nb_room);
-	calc_dstart(tab, matrix);
-	calc_dend(tab, matrix);
-	calc_total(tab);
-}i
+t_ant			**path_finder(int **matrix, t_anthill *ah)
+{
+	t_colony		**ant;
+	t_colony		**reached;
+	t_roadmap		*rmap;
+	size_t			nb_ant;
+
+	nb_ant = tube_count(matrix, 0);
+	ant = ant_spawn(nb_ant);
+	reached = NULL;
+	rmap = rmap_init();
+	while (eval(reached, ah) == PROCEED)
+	{
+		ant_loop(ant, reached, &nb_ant);
+		rmap_add(rmap, reached);
+		if (nb_ant == 0)
+			break ;
+	}
+	return (rmap);
+}
