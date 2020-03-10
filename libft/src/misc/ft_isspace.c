8abf3257cd3be52dfb30d2386515d3a45c9ee75b
Zaska Seignon <zseignon@z2r2p4.le-101.fr>
2020-03-10 11:29:23 +0100

    vvreyat tools added to libft

--- libft/src/misc/ft_isspace.c
+++ libft/src/misc/ft_isspace.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zseignon <zseignon@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/05 14:59:57 by zseignon          #+#    #+#             */
-/*   Updated: 2020/03/10 11:24:54 by zseignon         ###   ########lyon.fr   */
+/*   Created: 2020/03/10 11:16:47 by zseignon          #+#    #+#             */
+/*   Updated: 2020/03/10 11:16:51 by zseignon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <string.h>
-
-# define PTR_SIZE	8
-
-# define FD_STDIN	0
-# define FD_STROUT	1
-# define FD_STDERR	2
-
-# define INT_MIN	-2147483648
-# define INT_MAX	2147483647
-
-# include "ft_vector.h"
-# include "typedef.h"
-# include "ft_error.h"
-# include "ft_memman.h"
-# include "ft_cmp.h"
-# include "libc.h"
-# include "misc.h"
-
-#endif
+int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' ||
+					c == '\v' || c == '\f')
+		return (1);
+	return (0);
+}
--- libft/src/misc/nmatch.c
+++ libft/src/misc/nmatch.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   nmatch.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zseignon <zseignon@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/05 14:59:57 by zseignon          #+#    #+#             */
-/*   Updated: 2020/03/10 11:24:54 by zseignon         ###   ########lyon.fr   */
+/*   Created: 2020/03/10 11:23:03 by zseignon          #+#    #+#             */
+/*   Updated: 2020/03/10 11:27:47 by zseignon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <string.h>
-
-# define PTR_SIZE	8
-
-# define FD_STDIN	0
-# define FD_STROUT	1
-# define FD_STDERR	2
-
-# define INT_MIN	-2147483648
-# define INT_MAX	2147483647
-
-# include "ft_vector.h"
-# include "typedef.h"
-# include "ft_error.h"
-# include "ft_memman.h"
-# include "ft_cmp.h"
-# include "libc.h"
-# include "misc.h"
-
-#endif
+int		nmatch(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s2 == '*' && *s1 == '\0')
+		return (nmatch(s1, s2 + 1));
+	else if (*s2 == '*')
+		return (nmatch(s1 + 1, s2) + nmatch(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (nmatch(s1 + 1, s2 + 1));
+	return (0);
+}
