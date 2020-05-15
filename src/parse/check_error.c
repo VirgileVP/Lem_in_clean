dee8d4d390ed814ff3a59b7f86a8a9000ad23566
virgilepornet@gmail.com <virgilepornet@gmail.com>
2020-05-15 14:58:12 +0200

    fix parsing bugs

--- src/parse/check_error.c
+++ src/parse/check_error.c
@@ -115,7 +115,8 @@ int						check_error(t_read_room *pars)
 		if (pars->room[index][0] == '#'
 		&& check_sharp(pars, index, &order) == -1)
 			return (-1);
-		else if (check_definition(pars, &order, index) == -1)
+		else if (ft_stroccurence(pars->room[index], '-') > 1
+		|| check_definition(pars, &order, index) == -1)
 			return (-1);
 		index++;
 	}
